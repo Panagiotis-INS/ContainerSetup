b9b346e0d8be27e16ba2f292237d74a5dff5da23
Panagiotis_INS <39010461+Panagiotis-INS@users.noreply.github.com>
2025-07-13 16:41:57 +0300

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ apt install curl -y
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-x86_64.tar.gz
 gunzip nvim-linux-x86_64.tar.gz
 tar xvf nvim-linux-x86_64.tar
+rm /bin/nvim
 ln -s $(pwd)/nvim-linux-x86_64/bin/nvim /bin/nvim
 curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
 apt install nodejs -y
