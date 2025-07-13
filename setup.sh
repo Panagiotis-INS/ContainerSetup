4918397b98fab76450cdd0c4824a69684e28a175
Panagiotis_INS <39010461+Panagiotis-INS@users.noreply.github.com>
2025-07-13 16:35:57 +0300

    Minor fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 ##
 apt update
-apt install git -y
+apt install curl -y
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-x86_64.tar.gz
 gunzip nvim-linux-x86_64.tar.gz
 tar xvf nvim-linux-x86_64.tar
-ln -s /root/nvim-linux-x86_64/bin/nvim /bin/nvim
+ln -s $(pwd)/nvim-linux-x86_64/bin/nvim /bin/nvim
 curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
 apt install nodejs -y
 apt install npm -y
@@ -18,6 +18,5 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 ##
 mkdir .config
 mkdir .config/nvim
-wget https://raw.githubusercontent.com/Panagiotis-INS/ContainerSetup/refs/heads/main/init.vim
 mv ./init.vim ./.config/nvim
 ##
